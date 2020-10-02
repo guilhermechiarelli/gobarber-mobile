2bf1e33fe170f88715910c70e973454c11efbca6
Guilherme Chiarelli <guilherme.hchiarelli@gmail.com>
2020-10-01 22:50:27 -0300

    initial commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,63 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback, useEffect, useState } from 'react';
+import api from '../../services/api';
+
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+  ProvidersList,
+} from './styles';
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  const { user, signOut } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    // navigate('Profile');
+    signOut();
+  }, []);
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo,
+          {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+      />
+    </Container>
+  );
+};
+
+export default Dashboard;
